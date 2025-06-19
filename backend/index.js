4198c912da0d5c8e260b07fd58dbae05f1e606d6
Anupam Shukla <shuklaanupam987@gmail.com>
2025-06-20 02:24:31 +0530

    auth validation

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,34 @@
-import express from 'express'
+import express from "express";
 import dotenv from "dotenv";
-const app=express();
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
- const  PORT= process.env.PORT || 3001;
- const URI=process.env.MONGO_URI;
 
- try{
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 5001;
+const URI = process.env.MONGODB_URI;
+
+try {
   mongoose.connect(URI);
-  console.log("connected to MONGODB");
- }
-  catch(error){
-    console.log(error);
-  }
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.log(error);
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World !')
-})
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
+});
